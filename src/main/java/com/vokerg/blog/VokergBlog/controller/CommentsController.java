9e305ed0f99fb675a2a54112c0c2c2357fabfd79
Dmitri Grecov <vokerg@gmail.com>
2019-05-12 22:01:20 +0200

    adding comments endpoint

---
+++ src/main/java/com/vokerg/blog/VokergBlog/controller/CommentsController.java
@@ -0,0 +1,22 @@
+package com.vokerg.blog.VokergBlog.controller;
+
+import com.vokerg.blog.VokergBlog.model.CommentFull;
+import com.vokerg.blog.VokergBlog.service.CommentsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="api/comments")
+public class CommentsController {
+    @Autowired
+    CommentsService commentsService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<CommentFull>> getTopComments() {
+        return ResponseEntity.ok(commentsService.getTopFullComments());
+    }
+
+}
--- src/main/java/com/vokerg/blog/VokergBlog/service/CommentsService.java
+++ src/main/java/com/vokerg/blog/VokergBlog/service/CommentsService.java
@@ -3,6 +3,7 @@ package com.vokerg.blog.VokergBlog.service;
 import com.vokerg.blog.VokergBlog.model.CommentFull;
 import com.vokerg.blog.VokergBlog.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.*;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -10,8 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.match;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.project;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
 @Service
 public class CommentsService {
@@ -23,24 +23,39 @@ public class CommentsService {
     @Autowired
     MongoTemplate mongoTemplate;
 
-    public List<CommentFull> getFullCommentsForAuthorId(String idAuthor) {
+    private List<CommentFull> getLatestFullComments(Criteria criteria, Long queryLimit, Long querySkip) {
         LookupOperation lookupOperation = LookupOperation.newLookup()
                 .from("articles")
                 .localField("newid")
                 .foreignField("_id")
                 .as("articles");
 
-        ProjectionOperation project = Aggregation.project(COMMENT_FIELDS)
+        ProjectionOperation project1 = Aggregation.project(COMMENT_FIELDS)
                 .and(ConvertOperators.valueOf("idArticle").convertToObjectId()).as("newid");
+        ProjectionOperation project2 = Aggregation.project(COMMENT_FIELDS).and("articles").arrayElementAt(0).as("article");
+
+
 
         Aggregation aggregation = Aggregation
                 .newAggregation(
-                        match(Criteria.where("idAuthor").is(idAuthor)),
-                        project,
+                        match(criteria),
+                        sort(new Sort(Sort.Direction.DESC, "_id")),
+                        limit(queryLimit != null ? queryLimit : Long.MAX_VALUE),
+                        skip(querySkip != null ? querySkip : 0),
+                        project1,
                         lookupOperation,
-                        project(COMMENT_FIELDS).and("articles").arrayElementAt(0).as("article")
+                        project2
                 );
+
         AggregationResults<CommentFull> results = mongoTemplate.aggregate(aggregation, "comments", CommentFull.class);
         return results.getMappedResults();
+    }
+
+    public List<CommentFull> getFullCommentsForAuthorId(String idAuthor) {
+        return getLatestFullComments(Criteria.where("idAuthor").is(idAuthor), null, null);
+    }
+
+    public List<CommentFull> getTopFullComments() {
+        return getLatestFullComments(Criteria.where("_id").ne(null), Long.valueOf(10), null);
     }
 }
